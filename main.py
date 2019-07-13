91d391e940bc87c1fab7120b11c9845345aff527
Michael A. Chapman <chapmanm305@gmail.com>
2019-07-13 14:52:05 -0700

    Implement basic styles and get images rendering

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello(message=None):
+def home():
     return render_template('main.html')
+
+@app.route('/sales')
+def sales():
+    return render_template('sales.html')
